2b238841a7ed1eb840650a5e4b4487c6d32b9279
liule13 <LEO.LIU@OOCL.COM>
2025-09-12 14:55:47 +0800

    feat(todo): todo rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import './App.css';
+import {createContext, useContext, useReducer} from "react";
 
 export const initState = [
     {id: 1, text: "the first todo", done: false},
     {id: 2, text: "the second todo", done: false},
 ];
+export const TodoContext = createContext()
 
+function TodoItem(props) {
+    return <div className="todo-item">
+        {props.todo.text}
+    </div>;
+}
+
+function TodoGroup() {
+    const {state, dispatch} = useContext(TodoContext)
+    return <div>
+        {
+            state.map((item,index)=>{
+                return <TodoItem todo={item} key={index} index={index}></TodoItem>
+            })
+        }
+    </div>;
+}
+
+export function todoReducer(state, action) {
+    return state
+}
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const [state, dispatch] = useReducer(todoReducer, initState);
+    return (
+        <div>
+            <TodoContext.Provider value={{state, dispatch}}>
+                <TodoGroup/>
+            </TodoContext.Provider>
+        </div>
+    );
 }
 
 export default App;
