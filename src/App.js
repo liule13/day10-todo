ed1149047ba37bc49dad2b7b0667a2fafd9c5724
liule13 <LEO.LIU@OOCL.COM>
2025-09-12 14:40:55 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+export const initState = [
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: false},
+];
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
