0740513357beb0d27620c318cb2225f45b8d02be
liule13 <LEO.LIU@OOCL.COM>
2025-09-15 13:22:50 +0800

    fix: filter done item

--- src/pages/DoneListPage.js
+++ src/pages/DoneListPage.js
@@ -1,12 +1,20 @@
 import {TodoGroup} from "../components/TodoGroup";
-import {initState} from "../contexts/TodoContext";
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import {TodoItem} from "../components/TodoItem";
 
 export function DoneListPage() {
-    const doneTodos = initState.filter(done => done.done === true);
+    const { state } = useContext(TodoContext)
     return (
-        <div className="todo-list">
-            <h2>Todo List</h2>
-            <TodoGroup todos = {doneTodos}></TodoGroup>
+        <div className="todo-group">
+            {state.length === 0 ? (
+                <p>Add the things you need to do today...</p>
+            ) : (
+                state.filter(
+                    (item) => item.done === true
+                ).map((item) => (
+                    <TodoItem key={item.id} todo={item} />
+                )))}
         </div>
     );
 }
