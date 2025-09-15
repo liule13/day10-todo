c6ca5d9fe54cef78341d53b722eb5f7089f1a16a
liule13 <LEO.LIU@OOCL.COM>
2025-09-15 11:09:07 +0800

    refactor: extract router

--- src/App.js
+++ src/App.js
@@ -2,30 +2,9 @@ import {useReducer} from "react";
 import "./App.css";
 import {todoReducer} from "./reducers/TodoReducer";
 import {initState, TodoContext} from "./contexts/TodoContext";
-import {createBrowserRouter, RouterProvider} from "react-router";
-import {ErrorPage} from "./pages/ErrorPage";
-import {HomePage} from "./pages/HomePage";
-import {TodoDetailPage} from "./pages/TodoDetailPage";
-import {DefaultLayout} from "./layouts/DefaultLayout";
+import {RouterProvider} from "react-router";
+import {router} from "./routers/Router";
 
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <DefaultLayout/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                path: "/",
-                element: <HomePage/>
-            },
-            {
-                path: "/todos/:id",
-                element: <TodoDetailPage/>
-            }
-        ]
-    }
-])
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
