24a8907962cfa6e144e0179145c653eb0b5da0c2
liule13 <LEO.LIU@OOCL.COM>
2025-09-15 20:40:22 +0800

    feat: replace input and button to antd components in AddTodoForm

--- src/components/AddTodoItem.js
+++ src/components/AddTodoItem.js
@@ -1,6 +1,8 @@
 import {useState, useContext} from "react";
 import {TodoContext} from "../contexts/TodoContext";
 import {useTodoService} from "../useTodoService";
+import {Button, Input} from "antd";
+import {PlusOutlined} from "@ant-design/icons";
 
 export function AddTodoForm() {
     const {createTodo} = useTodoService();
@@ -22,14 +24,21 @@ export function AddTodoForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="add-todo-form">
-            <input
-                type="text"
+        <form onSubmit={handleSubmit} style={{ display: "flex", gap: "8px", alignItems: "center" }}>
+            <Input
+                placeholder="请输入待办事项..."
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
-                className="todo-input"
+                size="middle"
             />
-            <button type="submit" className="add-button">add</button>
+            <Button
+                htmlType="submit"
+                type="primary"
+                icon={<PlusOutlined />}
+                size="middle"
+            >
+                Add
+            </Button>
         </form>
     );
 }
